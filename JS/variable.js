da3b1ce433d00b8871deffb9c834a4d740132ca8
Devanshshr <122160515+Devanshshr@users.noreply.github.com>
2024-11-02 09:02:53 +0530

    Create variable.js

---
+++ JS/variable.js
@@ -0,0 +1,15 @@
+console.log("Hii JS");
+console.log("I love JS");
+age=20;
+name="Devansh Sharma;
+console.log(name);
+age=34;
+price=34.32;
+x=null;
+y=undefined;
+_123=12;
+$34ehf=23;
+console.log(x);
+console.log(y);
+// We can use letters , numbers , $ and _ in variables name but 
+// we can start only with letter or $ or _. 
