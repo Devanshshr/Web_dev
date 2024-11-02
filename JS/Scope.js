e5f3eab837559488dc611fc344733e1342429acd
Devanshshr <122160515+Devanshshr@users.noreply.github.com>
2024-11-02 09:04:08 +0530

    Create Scope.js

---
+++ JS/Scope.js
@@ -0,0 +1,9 @@
+{
+    let a=9;
+    console.log(a);
+}
+
+// console.log(a);
+// above line will give an error because this variable has scope till block only not outside block
+let a=5; // we are able to redefine it because above declaration has scope till that block only
+console.log(a);
